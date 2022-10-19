5087565316c0305d47e7fe2c16946f0ad4631d16
Luiza Rodrigues Lencina_ <luiza.lencina@totvs.com.br>
2022-10-18 22:38:58 -0300

    try intercal and one a one

--- Board.java
+++ Board.java
@@ -2,37 +2,56 @@ import java.io.IOException;
 
 public class Board {
 
-    private int t, p, c, cont;
+    private int length, p, c, cont, space;
+    private int[][] bPlaces;
 
-    public Board(int tam, int pigs, int chickens) {
-        this.t = tam;
+    public Board(int length, int pigs, int chickens) {
+        this.length = length;
         this.p = pigs;
         this.c = chickens;
         cont = 0;
+        bPlaces = new int[length][length];
+        space = 0;
     }
 
     public void start() throws IOException {
-        int[][] board = new int[this.t][this.t];
-        picnic(board, 1, 0, 0, 0, 0);
-        System.out.println("Tamanho do tabuleiro: " + this.t + "x" + this.t);
+        int[][] board = new int[this.length][this.length];
+        int x = (length * length);
+
+        if (this.c > x || this.p > x || (this.c + this.p) > x) {
+            System.out.println(" ");
+            System.out.println("Não é possivel uma solução para este caso!");
+            System.out.println(" ");
+        } else {
+            int isBig = this.p > this.c ? 1 : 2;
+            picnic(board, isBig, 0, 0, 0, 0);
+        }
+
+        System.out.println("Tamanho do tabuleiro: " + length + "x" + length);
         System.out.println("Quantidade de porquinhos: " + p);
         System.out.println("Quantidade de galinhas: " + c);
         System.out.println(" ");
         System.out.println("Quantidade de soluções possíveis: " + cont);
-        // boardPrinter(board);
+        System.out.println(" ");
     }
 
     public boolean picnic(int[][] b, int a, int xp, int yp, int xc, int yc) throws IOException {
-
         boolean isPig = (a == 1) ? true : false;
+        boolean intercal = false;
+        int animal = isPig ? 2 : 1;
+        // if (intercal) {
+        // a = this.p > this.c ? 1 : 2;
+        // } else {
+        // a = this.p < this.c ? 1 : 2;
+        // }
 
         int x = isPig ? xp : xc;
         int y = isPig ? yp : yc;
 
         if (this.p <= 0 && this.c <= 0) {
+            // boardPrinter(b);
+            // System.out.println(" ");
             cont++;
-            boardPrinter(b);
-            System.out.println(" ");
             return true;
         }
 
@@ -41,79 +60,119 @@ public class Board {
                 if (isSafe(b, i, j, a)) {
                     b[i][j] = a;
 
-                    if (a == 1 && this.c > 0) {
-                        xp = i;
-                        yp = j;
-                        this.p--;
-                        picnic(b, 2, xp, yp, xc, yc);
-                        this.p++;
-                    } else if (a == 2 && this.p > 0) {
-                        xc = i;
-                        yc = j;
-                        this.c--;
-                        picnic(b, 1, xp, yp, xc, yc);
-                        this.c++;
-                    } else if (this.p > 0 && this.c == 0) {
-                        this.p--;
-                        xp = i;
-                        yp = j;
-                        picnic(b, 1, xp, yp, xc, yc);
-                        this.p++;
+                    if (intercal) {
+                        if (isPig && this.c > 0) {
+                            xp = i;
+                            yp = j;
+                            this.p--;
+                            picnic(b, 2, xp, yp, xc, yc);
+                            this.p++;
+                        } else if (!isPig && this.p > 0) {
+                            xc = i;
+                            yc = j;
+                            this.c--;
+                            picnic(b, 1, xp, yp, xc, yc);
+                            this.c++;
+                        } else if (this.p > 0 && this.c == 0) {
+                            this.p--;
+                            xp = i;
+                            yp = j;
+                            picnic(b, 1, xp, yp, xc, yc);
+                            this.p++;
+                        } else {
+                            xc = i;
+                            yc = j;
+                            this.c--;
+                            picnic(b, 2, xp, yp, xc, yc);
+                            this.c++;
+                        }
                     } else {
-                        xc = i;
-                        yc = j;
-                        this.c--;
-                        picnic(b, 2, xp, yp, xc, yc);
-                        this.c++;
+                        if (isPig && this.p > 0) {
+                            xp = i;
+                            yp = j;
+                            this.p--;
+                            boolean pig = (this.p > 0) ? picnic(b, 1, xp, yp, xc, yc)
+                                    : picnic(b, 2, xp, yp, xc, yc);
+                            this.p++;
+                        } else {
+                            xc = i;
+                            yc = j;
+                            this.c++;
+                            picnic(b, 1, xp, yp, xc, yc);
+                            this.c--;
+                        }
                     }
 
-                    b[i][j] = 0;
+                    b[i][j] = 0; // backtrack
                 }
             }
             y = 0;
         }
-
         return false;
 
     }
 
-    boolean isSafe(int board[][], int row, int col, int a) {
+    boolean isSafe(int board[][], int row, int col, int a) throws IOException {
         int i, j;
 
-        if (board[row][col] != 0) {
+        if (board[row][col] != 0 || board[row][col] == a)
             return false;
+
+        for (i = 0; i < length; i++) {
+            if (board[row][i] != 0 && board[row][i] != a || board[i][col] != 0 && board[i][col] != a)
+                return false;
+            bPlaces[row][i] = a;
+            bPlaces[i][col] = a;
         }
 
-        for (i = 0; i < board.length; i++)
-            if (board[row][i] != 0 && board[row][i] != a) {
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] != 0 && board[i][j] != a)
                 return false;
-            }
 
-        for (i = 0; i < board.length; i++)
-            if (board[i][col] != 0 && board[i][col] != a) {
+            bPlaces[i][j] = a;
+        }
+
+        for (i = row, j = col; j >= 0 && i < length; i++, j--) {
+            if (board[i][j] != 0 && board[i][j] != a)
                 return false;
-            }
+            bPlaces[i][j] = a;
+        }
 
-        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-            if (board[i][j] != 0 && board[i][j] != a) {
+        for (i = row, j = col; j < length && i >= 0; i--, j++) {
+            if (board[i][j] != 0 && board[i][j] != a)
                 return false;
-            }
+            bPlaces[i][j] = a;
+        }
 
-        for (i = row, j = col; j >= 0 && i < board.length; i++, j--)
-            if (board[i][j] != 0 && board[i][j] != a) {
+        for (i = row, j = col; i < length && j < length; i++, j++) {
+            if (board[i][j] != 0 && board[i][j] != a)
                 return false;
-            }
+            bPlaces[i][j] = a;
+        }
 
-        for (i = row, j = col; i < board.length && j < board.length; i++, j++)
-            if (board[i][j] != 0 && board[i][j] != a) {
-                return false;
-            }
+        // System.out.println(" ");
+        // boardPrinter(bPlaces);
+        // System.out.println(" ");
 
-        for (i = row, j = col; j < board.length && i >= 0; i--, j++)
-            if (board[i][j] != 0 && board[i][j] != a) {
-                return false;
-            }
+        return true;
+    }
 
+    public boolean isSpace(int a) throws IOException {
+        int animal = (a == 1) ? this.p : this.c;
+
+        for (int i = 0; i < bPlaces.length; i++) {
+            for (int j = 0; j < bPlaces[i].length; j++) {
+                if (bPlaces[i][j] == 0) {
+                    space++;
+                }
+                bPlaces[i][j] = 0; // backtrack
+            }
+        }
+
+        if (animal > space)
+            return false;
+
+        space = 0;
         return true;
     }
 
--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // System.out.println("porquinhos" + " " + args[1] + " " + "galinhas" + " " +
-        // args[2]);
-        // Picnic l = new Picnic(Integer.parseInt(args[0]), Integer.parseInt(args[1]),
-        // Integer.parseInt(args[2]));
-        // l.start();
-
         Board x = new Board(Integer.parseInt(args[0]), Integer.parseInt(args[1]),
                 Integer.parseInt(args[2]));
+        SimpleDateFormat t = new SimpleDateFormat("hh:mm:ss,SSS");
+        long start = System.currentTimeMillis();
+        // Board x = new Board(8, 2,
+        // 24);
         x.start();
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.println("o metodo executou em " + t.format(elapsed));
     }
 }
