0455c2874d39841c541c536f1a727bf291d25aed
Luiza Rodrigues Lencina_ <luiza.lencina@totvs.com.br>
2022-10-19 09:35:32 -0300

    adiconando comentarios

--- Board.java
+++ Board.java
@@ -7,15 +7,19 @@ public class Board {
     private boolean intercal;
 
     public Board(int length, int pigs, int chickens) {
-        this.length = length;
-        this.p = pigs;
-        this.c = chickens;
-        cont = 0;
-        bPlaces = new int[length][length];
-        space = 0;
-        intercal = true;
+        this.length = length; // tamanho do tabuleiro
+        this.p = pigs; // quantidade de porquinhos
+        this.c = chickens; // quantidade de galinhas
+        cont = 0; // quantidade de maneiras únicas possíveis de dispor no tabuleiro
+        bPlaces = new int[length][length]; // matrjz de posições que ja não podem ser acessadas pelo animal oposto
+        space = 0; // quantidade de espaços livres ainda
+        intercal = true; // se faz intercalado (um porquino e depois uma galinha) ou se bota todos
+                         // primeiro e depois encaixa os outros
     }
 
+    // esse metodo apenas cria o tabuleito e faz uma mini-verificação de casos em
+    // que o resultado da 0 de cara
+    // caso contrário ele chama o pinic
     public void start() throws IOException {
         int[][] board = new int[this.length][this.length];
         int x = (length * length);
@@ -39,12 +43,26 @@ public class Board {
         System.out.println(" ");
     }
 
+    // metodo responsavel pela disposicao dos porcos e galinhas no tabuleiro
     public boolean picnic(int[][] b, int a, int xp, int yp, int xc, int yc) throws IOException {
+
+        // verifica se o animal mandado é um porqiunho
+        // caso !isPig, é uma galiha // a é o animal, logo se a == 1 é porco
         boolean isPig = (a == 1) ? true : false;
         int animal = isPig ? 2 : 1;
+
+        // caso ele for um porquinho, começa da ultima posicão do porquinho (linha e
+        // coluna) - > (x, y)
+        // xp = ultima posicão da linha do porquinho // yp = ultima posicao da linha dos
+        // porquinhos
+        // yp = ultima posicao da coluna do porquinho || yc = ultima posicao da linha
+        // das galinhas
         int x = isPig ? xp : xc;
         int y = isPig ? yp : yc;
 
+        // caso acabe os porquinhos e as galinhas (não tem mais o que botar) e encontrou
+        // a solução
+        // metodo cont++ (mais uma solucao) e retorna true
         if (this.p <= 0 && this.c <= 0) {
             // boardPrinter(b);
             // System.out.println(" ");
@@ -54,8 +72,15 @@ public class Board {
 
         for (int i = x; i < b.length; i++) {
             for (int j = y; j < b[i].length; j++) {
+                // ele pergunta "é de boa botar meu animal aqui ?"
                 if (isSafe(b, i, j, a)) {
+
+                    // caso sim ele adiciona o animal que eu quero
                     b[i][j] = a;
+
+                    // aqui ele verifica se precisa ser da forma intercalado ou botar todos de um
+                    // tipo e depois do outro
+                    // ambos metodos sao iguais so muda a verificação
 
                     if (intercal) {
                         if (isPig && this.c > 0) {
@@ -108,6 +133,10 @@ public class Board {
 
     }
 
+    // esse metodo verifica se ta tudo bem botar o porquinho na poxicao [x][y] do
+    // tabuleiro
+    // ele verifica se não tem nenuhma galinha na mesma coluna, linha e diagonal
+    // caso tiver ele não deixa rodar e tenta o proximo
     boolean isSafe(int board[][], int row, int col, int a) throws IOException {
         int i, j;
         int isBig = this.p < this.c ? 1 : 2;
@@ -154,6 +183,13 @@ public class Board {
         return true;
     }
 
+    // metodo para da quantidade de espaco
+    // ainda não sei onde botar essa validacao, teria que ser onde o intercal is
+    // false
+    // pq se eu botar todas os porquinhos e tiver menos espaço livre do que tem de
+    // galinhas, ele ja ignora essa solução e iria pra poxima
+    // mas não consegui achar onde exatamente e como exatamente fazer isso
+
     boolean isSpace(int a) throws IOException {
         int animal = (a == 1) ? this.p : this.c;
         space = 0;
@@ -174,6 +210,8 @@ public class Board {
         return true;
     }
 
+    // metodo resposavel apenas por printar de maneira "bonitinha" as matrizes com
+    // porquinhos e galinhas
     public static void boardPrinter(int[][] board) throws IOException {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
